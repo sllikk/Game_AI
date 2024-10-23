cd27015b443c918ee7277c42f89d052d7ed82782
$L1ick <145702260+sllikk@users.noreply.github.com>
2024-10-23 23:09:31 +0300

    Base Change custom character and MoveComponent

--- Source/AI_CORE/Private/Player/Custom_MovementComponent.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Player/Custom_MovementComponent.h"
-
-UCustom_MovementComponent::UCustom_MovementComponent()
-{
-
-}
---
+++ Source/AI_CORE/Public/Player/CustomMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "CustomMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AI_CORE_API UCustomMovementComponent : public UPawnMovementComponent
+{
+	GENERATED_BODY()
+	
+};
